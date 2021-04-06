06d235c833e184658d14e377db31ab5c53b16134
Alan-Musk <59758888+Alan-Musk@users.noreply.github.com>
2021-04-06 15:05:25 +0800

    第四周

---
+++ 中国大学MooC/翁恺老师_C语音/week04/循环的例子/example01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int x;
+    int ret=0;
+    x=128;
+    int t=x;
+    while(x>1){
+        x/=2;
+        ret++;
+    }
+    printf("log2 of %d is %d",t,ret);
+}
---
+++ 中国大学MooC/翁恺老师_C语音/week04/循环的例子/example02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int count=100;
+    while(count>=1){
+        count--;
+        printf("%d\n",count);
+    }
+    printf("发射!\n");
+    return 0;
+}
